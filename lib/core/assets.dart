e24f3e436d594fd70733e587f49d6a8c5c1bcdb8
KHajili98 <68163357+KHajili98@users.noreply.github.com>
2023-01-14 12:29:17 +0400

    chat page ready

---
+++ lib/core/assets.dart
@@ -0,0 +1,7 @@
+class Assets {
+  static const String activeSend = "assets/active_send.png";
+  static const String attach = "assets/attach.png";
+  static const String save = "assets/save.png";
+  static const String send = "assets/send.png";
+  static const String smile = "assets/smile.png";
+}
