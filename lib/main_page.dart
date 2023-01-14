5003cd052a742cc5275250cc7e3569226ba1041f
MuhammedSafaroff <seferovmehemmed0@gmail.com>
2023-01-14 10:47:08 +0400

    home page ready

---
+++ lib/main_page.dart
@@ -0,0 +1,116 @@
+import 'package:chat_dodo/chat_page.dart';
+import 'package:flutter/material.dart';
+import 'package:stomp_dart_client/stomp.dart';
+import 'package:stomp_dart_client/stomp_config.dart';
+import 'package:stomp_dart_client/stomp_frame.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String token1 =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJVc2VyIERldGFpbHMiLCJpc3MiOiJDaGF0ZG9kb0NvbnZlcnNhdGlvblNlcnZpY2UiLCJpZCI6IlVTNTdiZmY4YWQ1MmQ5NDkwYWI4ZTA0NGJiMGEyZjY5NWYiLCJleHAiOjE2NzYxOTQ2OTgsImlhdCI6MTY3MzYwMjY5OH0.w_755Oz4W7yzJWd7sf9j_R5f1hVwUKjYUDQNsC5qI6Y";
+  String token2 =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJVc2VyIERldGFpbHMiLCJpc3MiOiJDaGF0ZG9kb0NvbnZlcnNhdGlvblNlcnZpY2UiLCJpZCI6IlVTOGYxNTU5MDFjYzM5NGRmMGExNmE5YmE2OTY3YjJmZTciLCJleHAiOjE2NzYxOTQ3MTYsImlhdCI6MTY3MzYwMjcxNn0.IpT5foEK8DjRlst-j_pBK4lkTRW-vZsTnuxmgZNLiLo";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Material(
+              color: const Color(0xff694BFF),
+              borderRadius: BorderRadius.circular(8),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(8),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ChatPage(token: token1),
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'User 1',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Material(
+              color: const Color(0xff694BFF),
+              borderRadius: BorderRadius.circular(8),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(8),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ChatPage(token: token2),
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'User 2',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+// client.send(
+//                     destination:
+//                         '/ws/v1/conversations/CN9556bcc9a7154218a5d97ac572a35671/sendMessage',
+//                     // headers: {"Authorization": "Bearer $token"},
+//                     headers: {},
+//                     body: '{"body" : "Salam"}',
+//                   );
